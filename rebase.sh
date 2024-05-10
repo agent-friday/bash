eceda2fc71dfcc2dfcf4abc1b16c9a981bc40dee
Matt <72048549+agent-friday@users.noreply.github.com>
2024-05-10 17:31:54 -0600

    Update rebase.sh
    Better options parsing, auto detect what the default branch is, and add a force option

--- rebase.sh
+++ rebase.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-MAIN_BRANCH=main
-REV_COUNT=$(git rev-list $MAIN_BRANCH.. --count)
+HELP=0
+FORCE=0
+MAIN_BRANCH=$(git symbolic-ref /refs/remotes/origin/HEAD --short | sed 's@^origin/@@')
+BRANCH=
   
 function help() {
     echo "Usage: rebase [$MAIN_BRANCH]"
@@ -14,14 +16,55 @@ function help() {
     echo "Passing $MAIN_BRANCH as an argument will run the command"
     echo "'git rebase -i $MAIN_BRANCH'"
 }
+
+function getoptions() {
+    LONGOPTS=help,force
+    OPTIONS=hf
+
+    PARSED=$(/usr/local/opt/gnu-getopt/bin/getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+
+    eval set -- $PARSED
+
+    while true; do
+        case "$1" in
+            -h|--help)
+                HELP=1
+                shift
+                ;;
+            -f|--force)
+                FORCE=1
+                shift
+                ;;
+            --)
+                shift
+                break;
+                ;;
+            *)
+                echo "ERROR"
+                exit 3
+        esac
+    done
+
+    $BRANCH=$1
+}
   
 #############################################
 # MAIN
 #############################################
-  
-if [[ -z $1 ]]; then
+
+getoptions "$@"
+
+REV_COUNT=$(git rev-list $MAIN_BRANCH.. --count)
+
+if [[ $HELP -eq 1 ]]; then
+    help
+    exit 0
+fi
+
+if [[ -z $BRANCH ]]; then
     # Not rebasing form main branch
     if [[ $REV_COUNT -gt 1 ]]; then
+        pull.sh .
         git rebase -i HEAD~$REV_COUNT
     else
         echo "Commit count: $REV_COUNT -- Nothing to do."
@@ -29,7 +72,7 @@ if [[ -z $1 ]]; then
  
 elif [[ $1 == $MAIN_BRANCH ]]; then
     # Rebasing from main branch
-    if [[ $REV_COUNT -gt 1 ]]; then
+    if [[ $REV_COUNT -gt 1 && $FORCE -ne 1 ]]; then
         echo "There are $REV_COUNT commits to this branch. You should rebase the branch first."
     else
         echo "Rebaseing from $MAIN_BRANCH..."
